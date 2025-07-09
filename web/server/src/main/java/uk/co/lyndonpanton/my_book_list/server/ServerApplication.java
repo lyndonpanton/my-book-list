e56e56164e3cf7a0f9e56e13a08b9cc3a70804c9
Lyndon Panton <LyndonPanton@gmail.com>
2025-07-09 14:34:23 +0100

    Create server

---
+++ web/server/src/main/java/uk/co/lyndonpanton/my_book_list/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package uk.co.lyndonpanton.my_book_list.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ web/server/src/test/java/uk/co/lyndonpanton/my_book_list/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.lyndonpanton.my_book_list.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
